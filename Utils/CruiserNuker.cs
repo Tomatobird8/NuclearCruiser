0c6a119ff8b915ee5c99afb4dd1b3cca07d388ab
Yohomok <103838479+ScandalTheVandal@users.noreply.github.com>
2026-07-08 09:12:04 +0100

    Update CruiserNuker.cs

--- Utils/CruiserNuker.cs
+++ Utils/CruiserNuker.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 namespace NuclearCruiser.Utils;
 
@@ -6,45 +6,44 @@ public class CruiserNuker : MonoBehaviour
 {
     internal void Explode()
     {
-        GameObject? nuke = Instantiate(NuclearCruiser.nukeObject);
-        if (nuke != null) 
+        GameObject? nukeObject = Instantiate(NuclearCruiser.nukeObject);
+        if (nukeObject != null)
         {
-            nuke.transform.position = gameObject.transform.position;
+            nukeObject.transform.position = gameObject.transform.position;
         }
     }
-        
-        private void Start()
+
+    private void Start()
+    {
+        if (NuclearCruiser.nuclearCruiserRadiationWarning && !StartOfRound.Instance.inShipPhase)
         {
-            if (NuclearCruiser.nuclearCruiserRadiationWarning && !StartOfRound.Instance.inShipPhase)
-            {
-                HUDManager.Instance.RadiationWarningHUD();
-            }
-            if (NuclearCruiser.nuclearCruiserWarning && !StartOfRound.Instance.inShipPhase)
-            {
-                if (NuclearCruiser.infiniteBoosts)
-                {
-                    HUDManager.Instance.DisplayTip("NUCLEAR CRUISER SPAWNED", "Using nuclear power the cruiser can boost infinitely. Handle with extreme care.", true);
-                }
-                else
-                {
-                    HUDManager.Instance.DisplayTip("NUCLEAR CRUISER SPAWNED", "Handle with extreme care.", true);
-                }
-            }
-            VehicleController? vc = GetComponent<VehicleController>();
-            if (vc == null) return;
+            HUDManager.Instance.RadiationWarningHUD();
+        }
+        if (NuclearCruiser.nuclearCruiserWarning && !StartOfRound.Instance.inShipPhase)
+        {
             if (NuclearCruiser.infiniteBoosts)
             {
-                vc.turboBoosts = 5;
-                vc.AddTurboBoost();
+                HUDManager.Instance.DisplayTip("NUCLEAR CRUISER SPAWNED", "Using nuclear power the cruiser can boost infinitely. Handle with extreme care.", true);
             }
-            if (vc.vehicleID != 0) return;
-            MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
-            foreach (var renderer in meshRenderers)
+            else
             {
-                if (renderer.transform.name.Contains("MainBody") || renderer.transform.name == "CarHoodMesh" || renderer.transform.name == "Door")
-                {
-                    renderer.materials[0].mainTexture = NuclearCruiser.cruiserTexture;
-                }
+                HUDManager.Instance.DisplayTip("NUCLEAR CRUISER SPAWNED", "Handle with extreme care.", true);
+            }
+        }
+        VehicleController? vc = GetComponent<VehicleController>();
+        if (vc == null) return;
+        if (NuclearCruiser.infiniteBoosts)
+        {
+            vc.turboBoosts = 5;
+            vc.AddTurboBoost();
+        }
+        if (vc.vehicleID != 0) return;
+        MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        foreach (var renderer in meshRenderers)
+        {
+            if (renderer.transform.name.Contains("MainBody") || renderer.transform.name == "CarHoodMesh" || renderer.transform.name == "Door")
+            {
+                renderer.materials[0].mainTexture = NuclearCruiser.cruiserTexture;
             }
         }
     }
